dc14af34c50e5ed9e4864731c2d598a8d3fce315
dembskii <dembsky09@gmail.com>
2026-03-04 12:00:18 +0100

    Add caching to Cpu Specification Service

--- backend-spring-app/src/main/java/com/cpu/management/service/CpuSpecificationService.java
+++ backend-spring-app/src/main/java/com/cpu/management/service/CpuSpecificationService.java
@@ -6,6 +6,8 @@ import com.cpu.management.dto.CpuSpecificationDTO;
 import com.cpu.management.mapper.EntityMapper;
 import com.cpu.management.repository.CpuSpecificationRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,22 +28,26 @@ public class CpuSpecificationService {
     // METODY DTO - dla REST API
     // =====================================================
 
+    @CacheEvict(value = {"allSpecifications", "specifications"}, allEntries = true)
     public CpuSpecificationDTO addSpecification(CpuSpecificationCreateDTO specificationCreateDTO) {
         CpuSpecification specification = entityMapper.toCpuSpecificationEntity(specificationCreateDTO);
         CpuSpecification savedSpecification = cpuSpecificationRepository.save(specification);
         return entityMapper.toCpuSpecificationDTO(savedSpecification);
     }
 
+    @Cacheable(value = "specifications", key = "#id")
     public Optional<CpuSpecificationDTO> getSpecificationById(UUID id) {
         return cpuSpecificationRepository.findById(id).map(entityMapper::toCpuSpecificationDTO);
     }
 
+    @Cacheable(value = "allSpecifications")
     public List<CpuSpecificationDTO> getAllSpecifications() {
         List<CpuSpecification> specifications = new ArrayList<>();
         cpuSpecificationRepository.findAll().forEach(specifications::add);
         return entityMapper.toCpuSpecificationDTOList(specifications);
     }
 
+    @CacheEvict(value = {"allSpecifications", "specifications"}, key = "#id")
     public void deleteSpecificationById(UUID id) {
         cpuSpecificationRepository.deleteById(id);
     }
