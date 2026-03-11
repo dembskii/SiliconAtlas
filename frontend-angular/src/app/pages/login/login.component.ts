f52acfcac36546733aadcf2ea335cdf596314443
Dominik Dembski <dembsky09@gmail.com>
2026-03-11 23:13:58 +0100

    Add basic components for logging

---
+++ frontend-angular/src/app/pages/login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule, RouterLink],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  username = '';
+  password = '';
+
+  onSubmit(): void {
+    // TODO: implement login logic
+  }
+}
---
+++ frontend-angular/src/app/pages/register/register.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [FormsModule, RouterLink],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss'
+})
+export class RegisterComponent {
+  username = '';
+  email = '';
+  password = '';
+  confirmPassword = '';
+
+  onSubmit(): void {
+    // TODO: implement registration logic
+  }
+}
